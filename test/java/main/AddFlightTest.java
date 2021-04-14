8ee69347ca8d7091f85a62c580ace21d882980c1
mashafi3209 <54332351+mashafi3209@users.noreply.github.com>
2021-04-14 14:12:26 -0400

    Added AddFlightTest

---
+++ test/java/main/AddFlightTest.java
@@ -0,0 +1,31 @@
+package main;
+
+import java.io.IOException;
+import java.text.ParseException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AddFlightTest {
+
+  public Addflight addFlight = new Addflight();
+
+  @BeforeEach
+  public void initCustomer() throws IOException, ParseException {
+    addFlight.setID("1");
+    addFlight.setFlightName("American Airlines");
+    addFlight.setSource("USA");
+    addFlight.setDate("01 JAN 2001");
+    addFlight.setDepartTime("12:00");
+    addFlight.setArrTime("4:00");
+    addFlight.setFlightCharge("200");
+  }
+
+  @Test
+  public void jButtonAddActionPerformedTest(){
+    boolean result = addFlight.jButtonAddActionPerformed(null);
+    Assertions.assertTrue(result);
+  }
+
+}
+
