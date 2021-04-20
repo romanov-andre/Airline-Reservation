44f2f6c83a6f479230a7aa5e1089c38c038a8d70
INeedThisForClass <36318303+INeedThisForClass@users.noreply.github.com>
2021-04-19 21:11:41 -0400

    Update TicketReportTest.java

--- test/unit/main/TicketReportTest.java
+++ test/unit/main/TicketReportTest.java
@@ -1,9 +1,21 @@
 package main;
 
-
 public class TicketReportTest {
 
+	private Ticketreport testReport = new Ticketreport();
 
+	@Test
+	public void LoadDataTest() {
 
+		Assertions.assertTrue(testReport.LoadData());
+
+	}
+
+	@Test
+	public void cancelReportTest() {
+
+		Assertions.assertTrue(testReport.jButton1ActionPerformed(null));
+	}
 
 }
+
